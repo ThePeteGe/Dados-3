d70de2f9a13ad08a350c0adfc89d275bdd7f3b51
ThePeteGe <santiagoandresrey@gmail.com>
2025-09-04 17:11:36 -0500

    suma

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int n1,n2,suma;
+        System.out.println("Digite el priemr numero");
+        n1 = teclado.nextInt();
+        System.out.println("Digite el segundo numero");
+        n2 = teclado.nextInt();
+        suma = n1 + n2;
+        System.out.println("La suma es: " + suma);
+
+
+    }
+}
