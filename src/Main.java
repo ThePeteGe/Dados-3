ed7062137786bc5ba45e475badb930ef6dd3c73c
ThePeteGe <santiagoandresrey@gmail.com>
2025-09-04 17:55:14 -0500

    dados a y b

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,21 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        int n1,n2,suma;
-        System.out.println("Digite el priemr numero");
-        n1 = teclado.nextInt();
-        System.out.println("Digite el segundo numero");
-        n2 = teclado.nextInt();
-        suma = n1 + n2;
-        System.out.println("La suma es: " + suma);
 
         Random aleatorio = new Random();
-        int a,b,c ;
-        System.out.println("numero generado");
-        a=aleatorio.nextInt(51);
-        System.out.println("numero generado");
-        b=aleatorio.nextInt((30-5)+1)+5;
-        c= a + b ;
-        System.out.println("a + b = "+c);
+        int a,b;
+        int suma1=0;
+        int suma2=0;
+        for(int i=1;   i<=20; i++){
+        System.out.println("dado A generado");
+        a=aleatorio.nextInt(7-1)+1;
+        suma1+=a ;
+        System.out.println("dado B generado");
+        b=aleatorio.nextInt((7-1)+1);
+        suma2+=b ;
+        System.out.println("DADO A : " +suma1);
+        System.out.println("DADO B : " +suma2);
+        }
 
 
 
