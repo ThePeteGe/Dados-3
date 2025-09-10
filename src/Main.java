3e44bdf73e27c0b956c7250bc52c4926596a98ca
ThePeteGe <santiagoandresrey@gmail.com>
2025-09-10 15:25:06 -0500

    Dados 3

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,37 @@
 import java.util.Random;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         Random aleatorio = new Random();
-        int a,b;
+        int a,b,c;
         int suma1=0;
         int suma2=0;
+        int suma3=0;
         for(int i=1;   i<=20; i++){
-        System.out.println("dado A generado");
-        a=aleatorio.nextInt(7-1)+1;
+        a=aleatorio.nextInt(6)+1;
         suma1+=a ;
-        System.out.println("dado B generado");
-        b=aleatorio.nextInt((7-1)+1);
+        b=aleatorio.nextInt(6)+1;
         suma2+=b ;
+        c=aleatorio.nextInt(6)+1;
+        suma3+=c ;
+            System.out.println("------------------------------");
         System.out.println("DADO A : " +suma1);
         System.out.println("DADO B : " +suma2);
+            System.out.println("DADO C : "+suma3);
+            System.out.println("------------------------------");
+        }
+        if (suma1>suma2 && suma1>suma3) {
+            System.out.println("Dado A gana");
+        } else if (suma2>suma1 && suma2>suma3) {
+            System.out.println("Dado B gana");
+        } else if (suma3>suma1 && suma3>suma2) {
+            System.out.println("Dado C gana");
+        } else {
+            System.out.println("empate");
+
+
         }
 
 
